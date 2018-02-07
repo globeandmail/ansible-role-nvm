3c4bf6348b792e15461e7f0fa87f0f514168c1a6
1adam <1adam@users.noreply.github.com>
2018-02-07 12:10:56 -0500

    redir profile.d/nvm.sh src output to null

--- files/nvm.sh
+++ files/nvm.sh
@@ -1,4 +1,4 @@
 # Ansible managed
 export NVM_DIR="/usr/local/nvm"
-[ -s "$NVM_DIR/nvm.sh" ] && \. "$NVM_DIR/nvm.sh"  # This loads nvm
-[ -s "$NVM_DIR/bash_completion" ] && \. "$NVM_DIR/bash_completion"  # This loads nvm bash_completion
+[ -s "$NVM_DIR/nvm.sh" ] && \. "$NVM_DIR/nvm.sh" > /dev/null 2>&1  # This loads nvm
+[ -s "$NVM_DIR/bash_completion" ] && \. "$NVM_DIR/bash_completion" > /dev/null 2>&1  # This loads nvm bash_completion
